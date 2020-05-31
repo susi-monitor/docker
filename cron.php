ce8e94d04a1baa2183d663674616cc69b2b34379
Grzegorz Olszewski <grzegorz@olszewski.in>
2020-05-31 14:03:20 +0200

    Provide data update endpoint to cron

--- cron.php
+++ cron.php
@@ -13,8 +13,8 @@ foreach ($settings as $var) {
 }
 
 $cronEnabled = isset($_ENV['CRON_ENABLED']) ? $_ENV['CRON_ENABLED'] : false;
-$baseUrl = isset($_ENV['BASEURL']) ? $_ENV['BASEURL'] : false;
+$baseUrl = rtrim(isset($_ENV['BASEURL']) ? $_ENV['BASEURL'] : false, '/');
 
 if ($cronEnabled == true) {
-    echo "[CRON] Checking status of targets - status: ".shell_exec('curl -s '.$baseUrl);
+    echo "[CRON] Checking status of targets - status: ".shell_exec('curl -s '.$baseUrl.'/data/update');
 }
